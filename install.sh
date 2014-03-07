0670e1cecfe1cdec4cb2488ca33c6fa656703fe8
Li-Ting Tsai <lt.tsai@hopebaytech.com>
2014-03-07 11:09:23 +0800

    wget zshrc based on distribution

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 
+if [-f /etc/lsb_release]; then
+    wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_ubuntu
+elif [-f /etc/rc.conf]; then
+    wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_bsd
+else 
+    wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_mac
+fi
 
-wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_ubuntu
+wget -O ~/.gitconfig https://raw.github.com/onemouth/dot-files/master/gitconfig
 wget -O ~/.vimrc https://raw.github.com/onemouth/dot-files/master/vimrc
+
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
