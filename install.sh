065ea54bbf90cc8e01d28ca7497a6474a2cb13c8
Li-Ting Tsai <and.liting@gmail.com>
2014-03-08 12:36:52 +0800

    use default tmux

--- install.sh
+++ install.sh
@@ -13,3 +13,5 @@ wget -O ~/.vimrc https://raw.github.com/onemouth/dot-files/master/vimrc
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
+
+rm -f ~/.tmuxrc
