2c2edffe416170f9868795428c18fcfac39b1ad5
Li-Ting Tsai <lt.tsai@hopebaytech.com>
2014-03-10 10:33:20 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-if [-f /etc/lsb_release]; then
+if [-f /etc/lsb-release]; then
     wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_ubuntu
 elif [-f /etc/rc.conf]; then
     wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_bsd
 else 
     wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_mac
 fi
+
 
 wget -O ~/.gitconfig https://raw.github.com/onemouth/dot-files/master/gitconfig
 wget -O ~/.vimrc https://raw.github.com/onemouth/dot-files/master/vimrc
