9857c6318ce876719dfe0ef8fd59b03afab70e50
Li-Ting Tsai <lt.tsai@hopebaytech.com>
2014-03-10 12:05:45 +0800

    test again

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+#git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-if [ -f "/etc/lsb-release" ]; then
-    wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_ubuntu
-elif [ -f "/etc/rc.conf" ]; then
-    wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_bsd
-else 
-    wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_mac
-fi
+#if [ -f "/etc/lsb-release" ]; then
+#    wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_ubuntu
+#elif [ -f "/etc/rc.conf" ]; then
+#    wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_bsd
+#else 
+#    wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_mac
+#fi
 
 
-#wget -O ~/.gitconfig https://raw.github.com/onemouth/dot-files/master/gitconfig
+wget -O ~/.gitconfig https://raw.github.com/onemouth/dot-files/master/gitconfig
 wget -O ~/.vimrc https://raw.github.com/onemouth/dot-files/master/vimrc
 
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
 
-echo "" > ~/.tmuxrc
+echo "" > ~/.tmux.conf
