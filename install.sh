dff97a02275a4f0c623c6bbdcfcf7b64c51967e2
Li-Ting Tsai <lt.tsai@hopebaytech.com>
2014-03-10 11:21:14 +0800

    fight with sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-if [ -f /etc/lsb-release] then
+if [ -f "/etc/lsb-release" ]; then
     wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_ubuntu
-elif [ -f /etc/rc.conf] then
+elif [ -f "/etc/rc.conf" ]; then
     wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_bsd
 else 
     wget -O ~/.zshrc https://raw.github.com/onemouth/dot-files/master/zshrc_mac
@@ -16,4 +16,4 @@ wget -O ~/.vimrc https://raw.github.com/onemouth/dot-files/master/vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
 
-rm -f ~/.tmuxrc
+echo "" > ~/.tmuxrc
